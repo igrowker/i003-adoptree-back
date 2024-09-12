f6b2e0124bca2dea96da06ca61770edbfa3521f9
Matisayas <sayasmatias1@gmail.com>
2024-09-12 01:24:43 -0300

    migrate to nestJs

---
+++ src/main.ts
@@ -0,0 +1,46 @@
+// main.ts
+
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { PrismaService } from "nestjs-prisma";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // log query events
+  if (process.env.DB_LOGS === "true") {
+    const prismaService: PrismaService = app.get(PrismaService);
+    prismaService.$on("query", (e) => {
+      let queryString = e.query;
+
+      const params: any[] = JSON.parse(e.params);
+
+      params.forEach((param: any, index: number) => {
+        queryString = queryString.replace(
+          `$${index + 1}`,
+          typeof param === "string" ? `'${param}'` : param
+        );
+      });
+
+      console.log(
+        `[${e.timestamp}] Query: ${queryString} Duration: ${e.duration}ms \n`
+      );
+    });
+  }
+
+  app.enableCors({
+    credentials: true,
+    origin: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Iniciar la aplicación
+  await app.listen(process.env.PORT || 3000);
+
+  console.log(`App running: http://localhost:${process.env.PORT || 3000}`);
+}
+
+bootstrap();
