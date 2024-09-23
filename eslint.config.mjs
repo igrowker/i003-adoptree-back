819b45eb267742c5b98840c2df02939667776c3d
Matisayas <sayasmatias1@gmail.com>
2024-09-23 01:59:52 -0300

    fix eslint conflict

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
@@ -8,5 +7,4 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ];
