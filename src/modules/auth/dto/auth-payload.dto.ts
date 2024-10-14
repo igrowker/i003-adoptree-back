c1cde4233d99751c4105de3979c8199098ba1dc0
Victor Emanuel Cuello <84103575+emacuello@users.noreply.github.com>
2024-10-13 22:55:09 -0300

    Se modifico un poco la interface porque los datos no coincidian con lo recibido en db

--- src/modules/auth/dto/auth-payload.dto.ts
+++ src/modules/auth/dto/auth-payload.dto.ts
@@ -2,14 +2,14 @@ import { RoleEnum, User } from "@prisma/client";
 import { IsEnum } from "class-validator";
 
 export class AuthPayloadDTO {
-  avatar?: File;
+  avatar?: File | null | string;
   email: string;
-  id: string;
+  id: string | number;
   name: string;
   phone?: string;
   token: string;
-  arbolId: number | null;
-  googleId?: string;
+  arbolId?: number | null;
+  googleId?: string | null;
 
   @IsEnum(RoleEnum)
   role: RoleEnum;
