5c5b96979d1e5de0bcc6547305d8acba418a0c6c
Victor Emanuel Cuello <84103575+emacuello@users.noreply.github.com>
2024-10-14 15:57:09 -0300

    Agregando loggers para las metricas de Loki

--- src/modules/metrics/metrics.service.ts
+++ src/modules/metrics/metrics.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectMetric } from '@willsoto/nestjs-prometheus';
 import { Counter, Histogram } from 'prom-client';
 
@@ -9,13 +9,31 @@ export class MetricsService {
 		private readonly httpRequestCounter: Counter<string>,
 		@InjectMetric('http_request_duration_seconds')
 		private readonly httpRequestDurationHistogram: Histogram<string>
-	) {}
+	) {
+		// Log para verificar que las métricas se inician
+		console.log('Metrics initialized:', {
+			httpRequestCounter: this.httpRequestCounter,
+			httpRequestDurationHistogram: this.httpRequestDurationHistogram,
+		});
+	}
+	logg = new Logger('MetricsService');
+
 	// Metodo para incrementar el contador de solicitudes HTTP
 	incrementHttpRequests(method: string, path: string, status: string) {
+		this.logg.log('Incrementing http_requests_total for', {
+			method,
+			path,
+			status,
+		});
 		this.httpRequestCounter.inc({ method, path, status });
 	}
 	// Metodo para registrar la duración de una solicitud HTTP
 	observeHttpRequestDuration(method: string, path: string, duration: number) {
+		this.logg.log('Observing http_request_duration_seconds for', {
+			method,
+			path,
+			duration,
+		});
 		this.httpRequestDurationHistogram.observe({ method, path }, duration);
 	}
 }
